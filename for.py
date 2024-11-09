6bf287b72ef6f30f00adaeb8f10ddcc5e1171a05
aigerimdev <kalygulovas@gmail.com>
2024-11-09 14:45:21 -0800

    start exercise

---
+++ for.py
@@ -0,0 +1,13 @@
+for x in range(1, 10):
+    print(x)
+    print(x*x)
+
+for letter in "cofee":
+    print(letter)
+
+for num in range(10):
+    print(num)
+for num in range(10, 20, 2):
+    print(num)
+
+
